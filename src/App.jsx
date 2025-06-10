487eb2cdfbcd71b74e64da559cba0d91c52636d2
enzirneo <enzo.bispo22@gmail.com>
2025-06-10 20:56:16 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'
+
+import Header from './components/Header/index.jsx';
+
+function App() {
+  return (
+    <>
+      <Header />
+      {/* Outras seções aqui */}
+    </>
+  );
+}
+
+export default App;
