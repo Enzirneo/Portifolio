b98c98c315ebead6ddd93046c15a02f3ad2b848d
enzirneo <enzo.bispo22@gmail.com>
2025-06-24 17:01:31 -0300

    modificação da apresentação dos projetos para um carrossel automatico

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,43 +1,102 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import ProjectCard from '../ProjectCard';
 import { useLanguage } from "../../context/LanguageContext";
-import "./Projects.css";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Mousewheel } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/autoplay';
+import 'swiper/css/mousewheel';
+import './Projects.css';
 
 export default function Projects() {
   const { menuLabels } = useLanguage();
+  const swiperRef = useRef(null);
+
+  const projects = [
+    {
+      image: "/images/Enlemar.png",
+      title: "Enlemar",
+      descriptionKey: "proj1-descricao",
+      link: null,
+    },
+    {
+      image: "/images/organo.png",
+      title: "Organo",
+      descriptionKey: "proj2-descricao",
+      link: "https://organo-zeta-blond.vercel.app/",
+    },
+    {
+      image: "/images/Galeria.png",
+      title: "Galeria de Fotos",
+      descriptionKey: "proj3-descricao",
+      link: "https://galeria-de-fotos-rosy-iota.vercel.app/",
+    },
+    {
+      image: "/images/Neuro.png",
+      title: "NeuroSync",
+      descriptionKey: "proj4-descricao",
+      link: "https://neurosync-ten.vercel.app/",
+    },
+  ];
+
+  const handleMouseEnter = () => {
+    const swiper = swiperRef.current;
+    if (swiper?.autoplay?.running === false) {
+      swiper.autoplay.start();
+    }
+  };
+
+  const handleMouseLeave = () => {
+    const swiper = swiperRef.current;
+    if (swiper?.autoplay?.running === true) {
+      swiper.autoplay.stop();
+    }
+  };
 
   return (
     <section className="projects-section" id="projetos">
       <div className="section-container">
         <h2 data-key="projetos-titulo">{menuLabels["projetos-titulo"]}</h2>
-        <p data-key="projetos-descricao" dangerouslySetInnerHTML={{ __html: menuLabels["projetos-descricao"] }}></p>
+        <p
+          data-key="projetos-descricao"
+          dangerouslySetInnerHTML={{ __html: menuLabels["projetos-descricao"] }}
+        ></p>
 
-        <div className="projects-grid">
-          <ProjectCard
-            image="/images/Neuro.png"
-            title="NeuroSync"
-            description={menuLabels["proj4-descricao"]}
-            link="https://neurosync-ten.vercel.app/"
-            dataKeyDesc="proj4-descricao"
-            dataKeyLink="proj4-link"
-          />
-          <ProjectCard
-            image="/images/organo.png"
-            title="Organo"
-            description={menuLabels["proj2-descricao"]}
-            link="https://organo-zeta-blond.vercel.app/"
-            dataKeyDesc="proj2-descricao"
-            dataKeyLink="proj2-link"
-          />
-          <ProjectCard
-            image="/images/Galeria.png"
-            title="Galeria de Fotos"
-            description={menuLabels["proj3-descricao"]}
-            link="https://galeria-de-fotos-rosy-iota.vercel.app/"
-            dataKeyDesc="proj3-descricao"
-            dataKeyLink="proj3-link"
-          />
-          
+        <div
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <Swiper
+            modules={[Autoplay, Mousewheel]}
+            slidesPerView={3}
+            spaceBetween={40}
+            loop={true}
+            centeredSlides={true}
+            speed={1200}
+            autoplay={{
+              delay: 4000,
+              disableOnInteraction: false,
+              pauseOnMouseEnter: false,
+            }}
+            mousewheel={{ forceToAxis: true }}
+            className="projects-swiper"
+            onSwiper={(swiper) => {
+              swiperRef.current = swiper;
+              swiper.autoplay.stop();
+            }}
+          >
+            {projects.map((project, index) => (
+              <SwiperSlide key={index}>
+                <ProjectCard
+                  image={project.image}
+                  title={project.title}
+                  description={menuLabels[project.descriptionKey]}
+                  link={project.link}
+                  dataKeyDesc={project.descriptionKey}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </div>
     </section>
